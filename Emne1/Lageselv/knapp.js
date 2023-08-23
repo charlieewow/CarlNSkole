a85cb074f5f4e8b477f961dad73007945ab2e1f4
charlieewow <carl.narvefors@hotmail.com>
2023-08-23 10:22:35 +0200

    Update

--- Emne1/Lageselv/knapp.js
+++ Emne1/Lageselv/knapp.js
@@ -6,23 +6,18 @@
     
    
     function init() {
-    // Convert string to primitve number using parseInt()
+    
      number = parseInt(output.innerText)
      
-    /**
-    * Start counter by adding any number to start counting.
-    * In this case the output starts from 0 so to immediately
-    * invoke the button to increment the counter add 1 to start 
-    * counting in natural number (counting numbers).
-    */  counter = number + 1
+      counter = number + 1
      
      function increment() {
-       // Increment counter
+       
        value = counter++
        output.innerText = value
       }
      
-      // Output the increment value for every click
+    
        button.onclick = increment
     }
    
---
+++ Emne1/Trafikklys/script.js
@@ -0,0 +1,45 @@
+function red() {
+    document.getElementById("red1").style.backgroundColor = "red";
+    document.getElementById("red1").style.opacity = "100%";
+    
+    document.getElementById("yellow1").style.backgroundColor = "black";
+    document.getElementById("yellow1").style.opacity = "10%";
+    document.getElementById("green1").style.backgroundColor = "black";
+    document.getElementById("green1").style.opacity = "10%";
+
+}
+
+function redyel() {
+    document.getElementById("red1").style.backgroundColor = "red";
+    document.getElementById("red1").style.opacity = "100%";
+    document.getElementById("yellow1").style.backgroundColor = "yellow";
+    document.getElementById("yellow1").style.opacity = "100%";
+
+    document.getElementById("green1").style.backgroundColor = "black";
+    document.getElementById("green1").style.opacity = "10%";
+
+}
+
+function green() {
+    document.getElementById("green1").style.backgroundColor = "green";
+    document.getElementById("green1").style.opacity = "100%";
+
+    document.getElementById("yellow1").style.backgroundColor = "black";
+    document.getElementById("yellow1").style.opacity = "10%";
+    document.getElementById("red1").style.backgroundColor = "black";
+    document.getElementById("red1").style.opacity = "10%";
+
+}
+
+function yellow() {
+    document.getElementById("yellow1").style.backgroundColor = "yellow";
+    document.getElementById("yellow1").style.opacity = "100%";
+    document.getElementById("green1").style.backgroundColor = "black";
+    document.getElementById("green1").style.opacity = "10%";
+    document.getElementById("red1").style.backgroundColor = "black";
+    document.getElementById("red1").style.opacity = "10%";
+
+}
+
+
+
